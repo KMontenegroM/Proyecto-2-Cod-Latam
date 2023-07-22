cd28689d55656f50fd74129920d49d0d138f27b1
KMontenegroM <karelis.montenegrom@gmail.com>
2023-07-22 18:16:47 -0300

    primer commit proyecto2

---
+++ JS/java.js
@@ -0,0 +1,49 @@
+
+
+const Contacto = document.querySelector  (".nav")
+
+Contacto.style.backgroundColor = "violet";
+
+function saludar(Profesor){
+    return "Hola"+ Profesor;
+
+}
+console.log(saludar("Edinson"));
+
+
+var fotoMostrada = "kare2";
+
+if (document.images ="kare2"){
+     var fotoMostrada = "kare3";
+     Kare2.Src="CSS/imagenes/kare2.jpg";
+     var imgChange= "kare2";
+     Kare3.Src="CSS/imagenes/kare3.jpg";
+
+    }
+
+    
+
+function primera(kare2){
+    if (document.images){
+        document[kare2].src=kare3.src;}
+    }
+
+function segunda(kare3){
+    if (document.imagens){
+        document[kare3].src=kare2.src;}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
